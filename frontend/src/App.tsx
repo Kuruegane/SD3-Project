0fcdbc56c8a53be1ac9c00de16cfb27492ac612a
Gabriel Freitas <71287983+Kuruegane@users.noreply.github.com>
2021-05-06 20:46:50 -0300

    Fixed App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,39 @@
 import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <>
+    
+    //Início Fragmentação
+    <> 
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3">Dashboard De Vendas </h1>
 
         <div className="row px-3">
-          <div className="cols-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
-        </div>
-        <div className="cols-sm-6">
-          <h5 className="text-center text-secondary">Total de Vendas</h5>
-          <DonutChart />
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Participação nas Vendas</h5>
+            <DonutChart />
+          </div>
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">Todas as Vendas</h2>
-        </div>
+          <h2 className="text-primary">Todas vendas</h2>
 
+        </div>
 
         <DataTable />
       </div>
       <Footer />
-    </> /*Fim Fragmentação*/
+
+    </> //Fim Fragmentação
   );
 }
 
