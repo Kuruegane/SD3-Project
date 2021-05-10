c738c57851c0adf949252c092874d9a1609b2b16
Gabriel Freitas <71287983+Kuruegane@users.noreply.github.com>
2021-05-09 21:10:08 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,33 +1,62 @@
-import { Seller } from "./seller"
+import axios from "axios";
+import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
-export type Sale = {
-    id: number;
-    visited: number;
-    deals: number;
-    amount: number;
-    date: string;
-    seller: Seller;
+function DataTable() { //const NavBar = () => { .........}
+
+    const[activePage, setactivePage] = useState(0);
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            })
+    }, [activePage]);
+
+    const changePage = (index: number) => {
+        setactivePage(index);
+    }
+
+    return (
+        //Início Fragmentação
+        <>
+        <Pagination page={page} onPageChange={changePage} />
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Vendedor</th>
+                            <th>Clientes visitados</th>
+                            <th>Negócios fechados</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {page.content?.map(item => (
+                            <tr key={item.id}>
+                                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount.toFixed(2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </> //Fim Fragmentação
+    );
 }
 
-export type SalePage = {
-    content?: Sale[];
-    last: boolean;
-    totalPages: number;
-    totalElements: number;
-    size?: number;
-    number: number;
-    first: boolean;
-    numberOfElements?: number;
-    empty?: boolean;
-}
-
-export type SaleSum = {
-    sellerName: string;
-    sum: number;
-}
-
-export type SaleSuccess = {
-    sellerName: string;
-    visited: number;
-    deals: number;
-}
+export default DataTable;
