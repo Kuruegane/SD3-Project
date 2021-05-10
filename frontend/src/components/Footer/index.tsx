5610dec5767e1ba1dea5c7c3833d3f5394d07ebe
Gabriel Freitas <71287983+Kuruegane@users.noreply.github.com>
2021-05-09 21:57:07 -0300

    Fixed Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() { //const Footer = () => { .........}
     return (
             <footer className="footer mt-auto py-3 bg-dark"> 
                 <div className="container">
-                    <p className="text-light">App desenvolvido por <a href="https://github.com/Kuruegane" style="color:	#FF8C00" target="_blank" rel="noreferrer">Gabriel Henrique de Freitas Batista</a> & <a href="https://www.linkedin.com/in/gabrielhfbatista/" style="color: #FF8C00" target="_blank" rel="noreferrer">Linkedin</a></p>
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/Kuruegane" style="color:	#FF8C00" target="_blank" rel="noreferrer">Gabriel Henrique de Freitas Batista</a> & <a href="https://www.linkedin.com/in/gabrielhfbatista/" target="_blank" rel="noreferrer">Linkedin</a></p>
                     <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior </small></p>
                 </div>
